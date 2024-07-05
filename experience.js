98218a7c2f118334d496f61d8c7ddf7bb97691d9
YarNight <yaroslav239@gmail.com>
2024-07-05 21:06:54 +0300

    convert images

--- experience.js
+++ experience.js
@@ -2,56 +2,62 @@ const experience = [
     {
         "id": "1",
         "name": "Game Memo",
-        "img": "assets/IMG/Memo.png",
+        "img": "assets/IMG/Memo.webp",
         "link": "https://regemler.github.io/Cards/"
     },
     {
         "id": "2",
         "name": "Clothing Store",
-        "img": "assets/IMG/ClothingStore.png",
+        "img": "assets/IMG/ClothingStore.webp",
         "link": "https://regemler.github.io/ClothingStore/"
     },
     {
         "id": "3",
         "name": "Momentum",
-        "img": "assets/IMG/Mom.png",
+        "img": "assets/IMG/Mom.webp",
         "link": "https://github.com/REGEMLER/Momentum"
     },
     {
         "id": "4",
         "name": "Nonogram game",
-        "img": "assets/IMG/Nonogram.png",
+        "img": "assets/IMG/Nonogram.webp",
         "link": "https://github.com/REGEMLER/Nonogram"
-    },
-    {
-        "id": "5",
-        "name": "Coffee-house",
-        "img": "assets/IMG/coffee.png",
-        "link": "https://regemler.github.io/coffee/coffee-house/"
     },
     {
         "id": "6",
         "name": "Library",
-        "img": "assets/IMG/library.png",
+        "img": "assets/IMG/library.webp",
         "link": "https://regemler.github.io/library/library/"
     },
     {
         "id": "7",
         "name": "Cozy House",
-        "img": "assets/IMG/shelter.png",
+        "img": "assets/IMG/shelter.webp",
         "link": "https://regemler.github.io/Cozy-House/shelter/"
     },
     {
         "id": "8",
         "name": "Travel",
-        "img": "assets/IMG/Travel.png",
+        "img": "assets/IMG/Travel.webp",
         "link": "https://regemler.github.io/Travel/"
     },
     {
         "id": "9",
         "name": "Minesweeper",
-        "img": "assets/IMG/minesweeper.png",
+        "img": "assets/IMG/minesweeper.webp",
         "link": "https://regemler.github.io/Minesweeper/minesweeper/"
+    },
+    {
+        "id": "10",
+        "name": "SOS",
+        "img": "assets/IMG/SOS.webp",
+        "link": "https://regemler.github.io/SOS/"
+    },
+    {
+        "id": "11",
+        "name": "Puzzle-English",
+        "img": "assets/IMG/English.webp",
+        "link": "https://regemler.github.io/Puzzle-English/"
     },
 ]
 
